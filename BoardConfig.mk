db9598cf103e112cb08e780c2e733f046076433d
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 17:29:02 +0200

    a25x: vintf: Move FCM entries to common tree
    We also have a26xs which also has aidl vaultkeeper (read: vaultshitter) and hermes
    
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,3 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 
 ## SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
-## Manifest
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/device_compatibility_matrix.xml
